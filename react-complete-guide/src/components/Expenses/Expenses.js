e749cc7371b716a1255982475df06dbbbd1c034d
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-07-05 02:55:53 -0300

    communication between components, lifting and controlled vs uncontrolled components

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
-        <Card className="expenses">
-            {
-                props.items?.map(item => (
-                    <ExpenseItem key={item.id}
-                        title={item.title}
-                        amount={item.amount}
-                        date={item.date}
-                    />
-                ))
-            }
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter selectedValue={filteredYear} onChangeFilter={filterChangeHandler} />
+                {
+                    props.items?.map(item => (
+                        <ExpenseItem key={item.id}
+                            title={item.title}
+                            amount={item.amount}
+                            date={item.date}
+                        />
+                    ))
+                }
+            </Card>
+        </div>
+
     )
 }
 export default Expenses;
