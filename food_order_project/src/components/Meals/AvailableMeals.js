bb795e1c01a609f6ffeba42255e5473731fbe10c
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-08-04 20:28:03 -0300

    Adding Individual Meal Items & Displaying Them

--- food_order_project/src/components/Meals/AvailableMeals.js
+++ food_order_project/src/components/Meals/AvailableMeals.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import { DUMMY_MEALS } from './dummy-meals';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+    />);
     return (
         <section className={classes.meals}>
-            <ul>{mealsList}</ul>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
         </section>
     );
 };
