7df29d086ddb6548579896bb6144b6bf4b46484c
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-08-04 20:02:41 -0300

    Adding Meals component in food order

---
+++ food_order_project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './AvailableMeals.module.css';
+import { DUMMY_MEALS } from './dummy-meals';
+
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    return (
+        <section className={classes.meals}>
+            <ul>{mealsList}</ul>
+        </section>
+    );
+};
+
+export default AvailableMeals;
