4a7faf8aa4828e58c3564bfdd39cf28120f1cf72
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-08-14 16:19:00 -0300

    Validating using OnBlurrReact To Lost Focus

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -10,6 +10,14 @@ const SimpleInput = (props) => {
     setEnterName(event.target.value);
   }
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if (enterName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return;
+    }
+  }
+
   const formSubmissionHandler = event => {
     event.preventDefault();
     setEnteredNameTouched(true);
@@ -27,7 +35,13 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses} >
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type='text' id='name'
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enterName}
+        />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
