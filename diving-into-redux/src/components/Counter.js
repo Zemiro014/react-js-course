d6644f2cb7e030811dbaa18e32e764261aead1db
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-08-16 22:13:29 -0300

    Using Redux Data in React Components and  Dispatching Actions From Inside Components

--- diving-into-redux/src/components/Counter.js
+++ diving-into-redux/src/components/Counter.js
@@ -1,12 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incrementHander = () => {
+    dispatch({type: 'increment'})
+  }
+
+  const decrementHander = () => {
+    dispatch({type: 'decrement'})
+  } 
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHander}>Increment</button>
+        <button onClick={decrementHander}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
