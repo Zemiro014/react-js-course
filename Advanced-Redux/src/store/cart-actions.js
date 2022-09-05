499382964032a82d3e8275b7997999b772a8de3a
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-04 21:43:21 -0300

    Finalizing the Fetching Logic

--- Advanced-Redux/src/store/cart-actions.js
+++ Advanced-Redux/src/store/cart-actions.js
@@ -15,7 +15,10 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartSliceActions.replaceCart(cartData));
+            dispatch(cartSliceActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity,
+            }));
         } catch (error) {
             dispatch(uiSliceActions.showNotification({
                 status: 'error',
@@ -39,7 +42,10 @@ export const sendCartData = (cart) => {
         const sendRequest = async () => {
             const response = await fetch('https://react-http-course-190bc-default-rtdb.firebaseio.com/cart.json', {
                 method: 'PUT',
-                body: JSON.stringify(cart)
+                body: JSON.stringify({
+                    items: cart.items,
+                    totalQuantity: cart.totalQuantity,
+                })
             })
 
             if (!response.ok) {
