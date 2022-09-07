0ad64f73cb3dc7cb86e642de2e310083ab10899e
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-07 12:33:08 -0300

    Adding Dummy Data & More Content

--- react_router_practice/src/pages/AllQuotes.js
+++ react_router_practice/src/pages/AllQuotes.js
@@ -1,4 +1,16 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is fun 01!' },
+    { id: 'q2', author: 'Pedro', text: 'Learning React is fun 02!' },
+    { id: 'q3', author: 'Jerónimo', text: 'Learning React is fun 03!' },
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return (
+        <>
+            <QuoteList quotes={DUMMY_QUOTES} />
+        </>
+    )
 }
 export default AllQuotes;
