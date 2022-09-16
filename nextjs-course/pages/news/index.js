cc73aaa88bc743e7fc8817c7d0e7a80b0751a673
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-16 19:53:18 -0300

    Linking Between Pages

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,8 +1,17 @@
 // our-domain.com/news
+import Link from "next/link";
 import React from "react";
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return (
+        <>
+            <h1>The News Page</h1>
+            <ul>
+                <li> <Link href="/news/nextjs-is-a-great-framework">NextJs Is A Great Framework</Link></li>
+                <li><a href="">Something Else</a></li>
+            </ul>
+        </>
+    )
 }
 
 export default NewsPage;
