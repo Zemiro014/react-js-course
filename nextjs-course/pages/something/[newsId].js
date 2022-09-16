95c1d51e68be6723a40fedc2e6aefd8ef318d719
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-16 19:32:12 -0300

    Extracting Dynamic Parameter Values

--- nextjs-course/pages/something/[newsId].js
+++ nextjs-course/pages/something/[newsId].js
@@ -1,7 +1,15 @@
 // our-domain.com/something/*
+import { useRouter } from "next/router";
 import React from "react";
 
 function DynamicPage() {
+    const router = useRouter();
+
+    console.log(router.query.newsId)
+    const newsId = router.query.newsId;
+    //send a request to the backend API
+    // to fetch the news item with newsId
+
     return <h1>The Dynamic Page</h1>
 }
 
