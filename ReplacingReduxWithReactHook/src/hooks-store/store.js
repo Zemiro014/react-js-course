3d6bbf5851b9509cdeea67898d4d2d6db5dfc427
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-20 10:41:22 -0300

    Getting Started with a Custom Hook as a Store

---
+++ ReplacingReduxWithReactHook/src/hooks-store/store.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+const useStore = () => {
+    const setState = useState(globalState)[1];
+    
+    useEffect(() => {
+        listeners.push(setState);
+    }, [])
+};
