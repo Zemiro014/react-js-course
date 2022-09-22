c12200d738572ff684d9a544de04b2edfebe1846
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2022-09-22 10:31:08 -0300

    Writing Our First Test

---
+++ Testing_React_App/src/components/Greeting.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting"
+
+
+test("renders Hello world as a text", () => {
+
+    //Arrange
+    render(<Greeting />)
+
+    //Act
+    // ... nothing
+
+    //Asset
+    const helloWordElement = screen.getByText('Hello World', {exact: false})
+    expect(helloWordElement).toBeInTheDocument();
+})
